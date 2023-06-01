3f47c99dbc530b4b6caefa283bd9bc871e110ff6
DPS2004 <10176105+DPS2004@users.noreply.github.com>
2023-06-01 18:27:37 -0400

    start wanita

--- level.lua
+++ level.lua
@@ -2,11 +2,12 @@
 -- (discovered by a mom)
 
 level:setuprooms()
-level:reorderrooms(0,2,0,1,3)
+level:reorderrooms(0,3,2,0,1)
 
 windowroom = level.rooms[0]
 bgroom = level.rooms[1]
 decoroom = level.rooms[2]
+wanitaroom = level.rooms[3]
 
 ontop = level.rooms[4]
 
@@ -14,8 +15,11 @@ row0 = level.rows[0]
 row1 = level.rows[1]
 row2 = level.rows[2]
 
+wanitachar = level.rows[3]
+
 row1:setvisibleatstart(false)
 row2:setvisibleatstart(false)
+wanitachar:setvisibleatstart(false)
 
 
 -- window functions
@@ -185,6 +189,62 @@ windowroom:settheme(35,'NeonMuseum')
 row0:move(35,{x=10,y=70,pivot=0})
 row1:show(35)
 row1:move(35,{x=10,y=30,pivot=0})
+
+wanitaroom:move(34,{x=75,y=-20,sx=50,sy=50})
+wanitaroom:setbg(34,"wanitawindow.png")
+wanitaroom:mask(34,"wanitawindow.png")
+wanitachar:move(34,{x=50,y=30,pivot=0,sx=3,sy=3})
+wanitachar:showchar(34)
+
+wanitaroom:move(35.5,{y=20},1,'outExpo')
+
+function wanitahop(b,y)
+	y = y or 30
+	
+	wanitachar:move(b - 0.2,{y=35},0.333,'outSine')
+	wanitachar:move(b + 0.133,{y=30},0.333,'inSine')
+end
+wanitahop(36.333)
+
+wanitachar:playexpression(38,'barely')
+wanitachar:move(39,{sx=-3},0.5,'outQuad')
+
+wanitachar:playexpression(40,'neutral')
+
+wanitaroom:move(40,{x=50},2,'outExpo')
+window:move(40,{y=70},2,'outExpo')
+
+hundo = level:newdecoration('hundo.png', 0, wanitaroom.index, 'hundo')
+hundo:hide(0)
+hundo:move(0,{x=-10,y=PY(126)})
+hundo:show(40)
+hundo:move(40,{x=PX(126)},1,'outExpo')
+hundo:move(43,{x=110},1,'inExpo')
+
+levitra = newpopup('levitra.png',-16)
+cialis = newpopup('cialis.png',-17)
+levitra:grow(45,30,60)
+wanitahop(45)
+cialis:grow(47,70,60)
+wanitahop(47)
+levitra:shrink(48)
+cialis:shrink(48)
+
+wanitaroom:move(47,{y=-20},1,'inQuad')
+wanitaroom:move(48,{x=-20,y=65})
+wanitaroom:move(48.01,{x=20},2,'outExpo')
+
+wanitaroom:setbg(48,'wanitanigeria.png')
+
+wanitachar:move(48,{sx=2,sy=2})
+wanitachar:playexpression(48,'barely')
+
+palace = level:newdecoration('palace.png', 0, wanitaroom.index, 'palace')
+palace:hide(0)
+palace:move(0,{x=-50})
+palace:show(48)
+palace:move(50,{x=50},1,'outExpo')
+
 
 for i=4,12 do
 	lp[i]:shrink(35.5)
