89be7817c010162592cd45198846b39a30f2982e
DPS2004 <10176105+DPS2004@users.noreply.github.com>
2023-06-04 17:48:24 -0400

    start on wanita polish

--- level.lua
+++ level.lua
@@ -166,23 +166,23 @@ lp[5] = newpopup('smallbusinessowner.png',-8)
 lp[4]:grow(19.5,20,80)
 lp[5]:grow(19.5,80,20)
 
-lp[6] = newpopup('smallpopup.png',-9)
-lp[7] = newpopup('smallpopup.png',-10)
+lp[6] = newpopup('ink.png',-9)
+lp[7] = newpopup('ink.png',-10)
 lp[6]:grow(23.5,50,80)
 lp[7]:grow(23.5,50,20)
 
-lp[8] = newpopup('smallpopup.png',-11)
-lp[9] = newpopup('smallpopup.png',-12)
+lp[8] = newpopup('prescription.png',-11)
+lp[9] = newpopup('prescription.png',-12)
 lp[8]:grow(27.5,80,80)
 lp[9]:grow(27.5,20,20)
 
-lp[10] = newpopup('smallpopup.png',-13)
-lp[11] = newpopup('smallpopup.png',-14)
+lp[10] = newpopup('replica.png',-13)
+lp[11] = newpopup('replica.png',-14)
 lp[10]:grow(31.5,80,50)
 lp[11]:grow(31.5,20,50)
 
 
-lp[12] = newpopup('smallpopup.png',-15)
+lp[12] = newpopup('doctorshatehim.png',-15)
 lp[12]:grow(33.5,50,50)
 
 windowroom:settheme(35,'NeonMuseum')
@@ -191,8 +191,9 @@ row1:show(35)
 row1:move(35,{x=10,y=30,pivot=0})
 
 wanitaroom:move(34,{x=75,y=-20,sx=50,sy=50})
-wanitaroom:setbg(34,"wanitawindow.png")
-wanitaroom:mask(34,"wanitawindow.png")
+wanitaroom:setbg(34,"wbg_mynameis.png")
+wanitaroom:setfg(34,"wanitafg.png")
+wanitaroom:mask(34,"wanitamask.png")
 wanitachar:move(34,{x=50,y=30,pivot=0,sx=3,sy=3})
 wanitachar:showchar(34)
 
@@ -206,13 +207,23 @@ function wanitahop(b,y)
 end
 wanitahop(36.333)
 
+wanitaroom:setbg(37.666,'wbg_youngandlonely.png')
+
 wanitachar:playexpression(38,'barely')
 wanitachar:move(39,{sx=-3},0.5,'outQuad')
 
+wanitaroom:setbg(39.666,'wbg_white.png')
 wanitachar:playexpression(40,'neutral')
 
 wanitaroom:move(40,{x=50},2,'outExpo')
 window:move(40,{y=70},2,'outExpo')
+
+for100 = newpopup('for100.png',-16)
+inusdonly = newpopup('inusdonly.png',-17)
+for100:grow(40,83,22)
+inusdonly:grow(42,83,22)
+for100:shrink(44)
+inusdonly:shrink(44)
 
 hundo = level:newdecoration('hundo.png', 0, wanitaroom.index, 'hundo')
 hundo:hide(0)
@@ -221,8 +232,8 @@ hundo:show(40)
 hundo:move(40,{x=PX(126)},1,'outExpo')
 hundo:move(43,{x=110},1,'inExpo')
 
-levitra = newpopup('levitra.png',-16)
-cialis = newpopup('cialis.png',-17)
+levitra = newpopup('levitra.png',-18)
+cialis = newpopup('cialis.png',-19)
 levitra:grow(45,30,60)
 wanitahop(45)
 cialis:grow(47,70,60)
